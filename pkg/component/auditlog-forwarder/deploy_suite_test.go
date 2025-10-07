acd341b83c62d52fa0d70f1ffd597096c55b3dcd
Dimitar Mirchev <dimitar.mirchev@sap.com>
2025-10-07 11:58:44 +0300

    Add tests for auditlog forwarder deployer

---
+++ pkg/component/auditlog-forwarder/deploy_suite_test.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package auditlogforwarder_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	testclock "k8s.io/utils/clock/testing"
+
+	secretsutils "github.com/gardener/gardener/pkg/utils/secrets"
+	"github.com/gardener/gardener/pkg/utils/test"
+)
+
+func TestAuditlogForwarder(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Component Auditlog Forwarder Suite")
+}
+
+var _ = BeforeSuite(func() {
+	DeferCleanup(test.WithVar(&secretsutils.GenerateRandomString, secretsutils.FakeGenerateRandomString))
+	DeferCleanup(test.WithVar(&secretsutils.GenerateKey, secretsutils.FakeGenerateKey))
+	DeferCleanup(test.WithVar(&secretsutils.Clock, testclock.NewFakeClock(time.Time{})))
+})
